bb2cd2eaf5e8f3696da060073de77391b48f91dd
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-05-16 15:22:53 +0530

    changed url

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.REACT_APP_URL,
     credentials: true,
   },
 });
